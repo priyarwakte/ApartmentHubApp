4d499000996201493ee4e815e78920b0951bfd10
priyarwakte <116417781+priyarwakte@users.noreply.github.com>
2022-10-22 00:30:23 -0500

    Apartment Hub Application

---
+++ app/java/ansarker/github/io/houserent/Availablity.java
@@ -0,0 +1,10 @@
+package ansarker.github.io.houserent;
+
+import ansarker.github.io.houserent.model.Rent;
+import ansarker.github.io.houserent.model.User;
+
+public class Availablity {
+
+    public static User currentUser;
+    public static Rent currentRent;
+}
